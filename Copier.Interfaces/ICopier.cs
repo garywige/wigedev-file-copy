030c0fffad5b8821e0a78d2c5d353884122e2317
garywige <91767400+garywige@users.noreply.github.com>
2022-05-14 15:20:19 -0700

    CopyCancelExecute implementation

---
+++ Copier.Interfaces/ICopier.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace WigeDev.Copier.Interfaces
+{
+    public interface ICopier
+    {
+        public Task Copy();
+        public void Cancel();
+    }
+}
---
+++ Tests/CopyCancelExecuteTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WigeDev.ViewModel.Implementations;
+
+namespace Tests
+{
+    [TestClass]
+    public class CopyCancelExecuteTests
+    {
+        private CopyCancelExecute sut;
+        private bool isError;
+        private FakeCopier copier;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            copier = new FakeCopier();
+            sut = new(copier);
+            isError = false;
+        }
+
+        [TestMethod]
+        public void ExecuteDoesntThrow()
+        {
+            try
+            {
+                sut.Execute();
+            }
+            catch
+            {
+                isError = true;
+            }
+
+            Assert.IsFalse(isError);
+        }
+
+        [TestMethod]
+        public void ExecuteCallsICopierCopy()
+        {
+            sut.Execute();
+            Assert.IsTrue(copier.WasCopyCalled);
+        }
+
+        [TestMethod]
+        public void ExecuteCallsCancelWhenCalledTwice()
+        {
+            sut.Execute();
+            sut.Execute();
+            Assert.IsTrue(copier.WasCancelCalled);
+        }
+    }
+}

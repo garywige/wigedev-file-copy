48f2dce8e15d65a45677235d7c9f9688a644b9bb
garywige <91767400+garywige@users.noreply.github.com>
2022-05-14 12:43:38 -0700

    PathValidator implementation

---
+++ Validation.Implementations/PathValidator.cs
@@ -0,0 +1,13 @@
+﻿using WigeDev.Model.Interfaces;
+using System.IO;
+
+namespace WigeDev.Validation.Implementations
+{
+    public class PathValidator : TextFieldValidator
+    {
+        public PathValidator(ITextField field): base(field)
+        { }
+
+        new public bool IsValid => base.IsValid && (new DirectoryInfo(field.Text)).Exists;
+    }
+}

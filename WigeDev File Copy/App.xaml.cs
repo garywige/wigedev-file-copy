abd6d47509802cd0438542f68d9de7922b1f4079
garywige <91767400+garywige@users.noreply.github.com>
2022-05-13 20:38:27 -0700

    MainWindow

--- WigeDev File Copy/App.xaml.cs
+++ WigeDev File Copy/App.xaml.cs
@@ -5,7 +5,20 @@ namespace WigeDev_File_Copy
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public sealed partial class App : Application
     {
+        private MainWindow window;
+
+        public App()
+        {
+            window = new MainWindow();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            window.Show();
+        }
     }
 }
---
+++ WigeDev File Copy/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WigeDev_File_Copy
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

a0e26154bc71d41a3708bbae5a2f5abdab81d6b7
garywige <91767400+garywige@users.noreply.github.com>
2022-05-13 20:16:26 -0700

    Solution/Project creation

---
+++ WigeDev File Copy/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace WigeDev_File_Copy
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

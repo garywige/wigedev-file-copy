fef4c5d33c2d88716972b83d2b1de91024f4081c
garywige <91767400+garywige@users.noreply.github.com>
2022-05-14 08:31:38 -0700

    ViewModel interfaces

---
+++ ViewModel.Interfaces/IViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+using WigeDev.Model.Interfaces;
+
+namespace WigeDev.ViewModel.Interfaces
+{
+    public interface IViewModel
+    {
+        public ITextField Source { get; set; }
+        public ITextField Destination { get; set; }
+        public ICommand CopyCancelCommand { get; set; }
+        public IList<string> Output { get; set; }
+    }
+}

b55174d46c9a06d912196dbad2ff831630dc527d
garywige <91767400+garywige@users.noreply.github.com>
2022-05-14 13:14:46 -0700

    FormValidator implementation

---
+++ Tests/FormValidatorTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WigeDev.Validation.Implementations;
+using WigeDev.Validation.Interfaces;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class FormValidatorTests
+    {
+        private FakeValidator fieldValidator;
+        private FormValidator sut;
+        private bool isError;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var validators = new List<IValidator>();
+            fieldValidator = new FakeValidator();
+            validators.Add(fieldValidator);
+            sut = new(validators);
+            isError = false;
+        }
+
+        [TestMethod]
+        public void IsValidDoesntThrow()
+        {
+            try
+            {
+                var output = sut.IsValid;
+            }
+            catch
+            {
+                isError = true;
+            }
+
+            Assert.IsFalse(isError);
+        }
+
+        [TestMethod]
+        public void IsValidTrueWhenFieldsValid()
+        {
+            fieldValidator.IsValid = true;
+            var result = sut.IsValid;
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsValidFalseWhenFieldsInvalid()
+        {
+            fieldValidator.IsValid = false;
+            var result = sut.IsValid;
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ Validation.Implementations/FormValidator.cs
@@ -0,0 +1,16 @@
+﻿using WigeDev.Validation.Interfaces;
+
+namespace WigeDev.Validation.Implementations
+{
+    public class FormValidator : IValidator
+    {
+        protected IList<IValidator> validators;
+
+        public FormValidator(IList<IValidator> validators)
+        {
+            this.validators = validators;
+        }
+
+        public bool IsValid => validators.Where(validator => !validator.IsValid).Count() == 0;
+    }
+}

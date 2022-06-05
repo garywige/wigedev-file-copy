fcaf92fedb7f43cfcff93f518fdf3480826988c8
garywige <91767400+garywige@users.noreply.github.com>
2022-06-04 20:15:09 -0700

    OutputWindowAdapter

---
+++ Tests/OutputWindowAdapterTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WigeDev.View.Implementations;
+
+namespace Tests
+{
+    [TestClass]
+    public class OutputWindowAdapterTests
+    {
+        private OutputWindowAdapter sut;
+        private bool isError;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            sut = new();
+            isError = false;
+        }
+
+        [TestMethod]
+        public void OutputDefaultNull()
+        {
+            var result = sut.Output;
+            Assert.IsNull(result);
+        }
+    }
+}
---
+++ View/Implementations/OutputWindowAdapter.cs
@@ -0,0 +1,19 @@
+﻿using WigeDev.View.Interfaces;
+
+namespace WigeDev.View.Implementations
+{
+    public class OutputWindowAdapter : IOutputWindowAdapter
+    {
+        public object? Output => null;
+
+        public void Show()
+        {
+            // TODO: window.Show();
+        }
+
+        public bool? ShowDialog()
+        {
+            // TODO: window.ShowDialog();
+        }
+    }
+}

56dabb8fc19261907aa0b99984ae006d80bef791
garywige <91767400+garywige@users.noreply.github.com>
2022-06-05 13:12:50 -0700

    app initialization refactoring

--- WigeDev File Copy/App.xaml.cs
+++ WigeDev File Copy/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Windows.Input;
 using System.Windows;
 using WigeDev.Cancellation.Implementations;
 using WigeDev.Copier.Implementations;
@@ -12,6 +14,8 @@ using WigeDev.View.Implementations;
 using WigeDev.View.Windows;
 using WigeDev.ViewModel.Implementations;
 using WigeDev.ViewModel.Interfaces;
+using WigeDev.Output.Interfaces;
+using WigeDev.Settings.Interfaces;
 
 namespace WigeDev_File_Copy
 {
@@ -20,77 +24,19 @@ namespace WigeDev_File_Copy
     /// </summary>
     public sealed partial class App : Application
     {
-        private MainWindow window;
+        private Window window;
+        private IJobStatus jobStatus;
+        private ITextField sourceTF;
+        private ITextField destTF;
+        private IValidator[] validators;
+        private IOutput output;
+        private ISelectControlViewModel<ICopyStrategy> overwriteVM;
+        private ISettingsManager settingsManager;
+        private INotifyList<ICopyJobControlViewModel> jobList;
 
         public App()
         {
-            // TextFields
-            var source = new TextField();
-            var dest = new TextField();
-
-            // Validators
-            var validators = new IValidator[2];
-            validators[0] = new PathValidator(source);
-            validators[1] = new PathValidator(dest);
-
-            // Output
-            var outputList = new NotifyList<string>(new NotifyListEnumerator<string>());
-            var output = new BasicOutput(outputList);
-            var jobStatus = new JobStatus();
-
-            // SettingsManager
-            ObservableCollection<ICopyStrategy> copyStrategies = new();
-            copyStrategies.Add(new AllCopyStrategy(output));
-            copyStrategies.Add(new NoneCopyStrategy(output));
-            copyStrategies.Add(new OldCopyStrategy(output));
-            var overwriteVM = new OverwriteSelectControlViewModel<ICopyStrategy>("Overwrite Mode", copyStrategies);
-            var settingsManager = new SettingsManager(overwriteVM);
-
-            // CopyCancelCommand
-            var copyCancelCommand = new CopyCancelCommand(
-                new FormValidator(validators),
-                new CopyCancelExecute(
-                    new Copier(
-                    new FileEnumerator(settingsManager),
-                    source,
-                    dest,
-                    output,
-                    new PathConstructor(),
-                    new CancellationManager(),
-                    jobStatus), 
-                    jobStatus));
-
-            source.PropertyChanged += (s, e) => copyCancelCommand.TestCanExecute();
-            dest.PropertyChanged += (s, e) => copyCancelCommand.TestCanExecute();
-
-            // batch job list
-            var jobList = new NotifyList<ICopyJobControlViewModel>(new NotifyListEnumerator<ICopyJobControlViewModel>());
-
-            // AddJobExecute
-            var addJobExecute = new AddJobExecute(new OutputWindowFactory(
-                o => { }, 
-                o => 
-                {
-                    // TODO: view model and pass output
-                    var window = new AddJobWindow();
-                    if(window.ShowDialog() == true)
-                    {
-                        return true;
-                    }
-                    return false; 
-                }), 
-                jobList);
-
-            // Main Window
-            window = new MainWindow(
-                new FolderSelectionControlViewModel("Source", source, jobStatus, new BrowseCommand(new FolderBrowserDialogAdapter())),
-                new FolderSelectionControlViewModel("Destination", dest, jobStatus, new BrowseCommand(new FolderBrowserDialogAdapter())),
-                new CopyCancelCommandControlViewModel(jobStatus, copyCancelCommand),
-                new OutputViewModel(output, jobStatus),
-                overwriteVM,
-                null,
-                new CommandControlViewModel("Add Job", new Command(() => true, () => addJobExecute.Execute())),
-                new BatchListControlViewModel(jobList));
+            window = initializeWindow();
         }
 
         protected override void OnStartup(StartupEventArgs e)
@@ -98,6 +44,136 @@ namespace WigeDev_File_Copy
             base.OnStartup(e);
 
             window.Show();
+        }
+
+        private Window initializeWindow()
+        {
+            initTextFields();
+            initValidators();
+            initOutput();
+            initJobStatus();
+            initOverwriteVM();
+            initSettingsManager();
+            initBatchJobList();
+
+            // Main Window
+            return new MainWindow(
+                initFolderSelectionControlVM("Source", sourceTF),
+                initFolderSelectionControlVM("Destination", destTF),
+                initCopyCancelCommandControlVM(initCopyCancelCommand()),
+                initOutputVM(),
+                overwriteVM,
+                initStartBatchCCVM(),
+                initAddJobCCVM(),
+                initBatchListCVM());
+        }
+
+        private IFolderSelectionControlViewModel initFolderSelectionControlVM(string labelContent, ITextField textField)
+        {
+            return new FolderSelectionControlViewModel(labelContent, textField, jobStatus, new BrowseCommand(new FolderBrowserDialogAdapter()));
+        }
+
+        private ICommandControlViewModel initCopyCancelCommandControlVM(ICommand command)
+        {
+            return new CopyCancelCommandControlViewModel(jobStatus, command);
+        }
+
+        private IOutputViewModel initOutputVM()
+        {
+            return new OutputViewModel(output, jobStatus);
+        }
+
+        private void initOverwriteVM()
+        {
+
+            ObservableCollection<ICopyStrategy> copyStrategies = new();
+            copyStrategies.Add(new AllCopyStrategy(output));
+            copyStrategies.Add(new NoneCopyStrategy(output));
+            copyStrategies.Add(new OldCopyStrategy(output));
+            overwriteVM = new OverwriteSelectControlViewModel<ICopyStrategy>("Overwrite Mode", copyStrategies);
+        }
+
+        private ICommandControlViewModel initStartBatchCCVM()
+        {
+            return null;
+        }
+
+        private ICommandControlViewModel initAddJobCCVM()
+        {
+            var addJobExecute = new AddJobExecute(new OutputWindowFactory(
+                o => { },
+                o =>
+                {
+                    // TODO: view model and pass output
+                    var window = new AddJobWindow();
+                    if (window.ShowDialog() == true)
+                    {
+                        return true;
+                    }
+                    return false;
+                }),
+                jobList);
+            return new CommandControlViewModel("Add Job", new Command(() => true, () => addJobExecute.Execute()));
+        }
+
+        private IBatchListControlViewModel initBatchListCVM()
+        {
+            return new BatchListControlViewModel(jobList);
+        }
+
+        private void initBatchJobList()
+        {
+            jobList = new NotifyList<ICopyJobControlViewModel>(new NotifyListEnumerator<ICopyJobControlViewModel>());
+        }
+
+        private ICommand initCopyCancelCommand()
+        {
+            var copyCancelCommand = new CopyCancelCommand(
+                new FormValidator(validators),
+                new CopyCancelExecute(
+                    new Copier(
+                    new FileEnumerator(settingsManager),
+                    sourceTF,
+                    destTF,
+                    output,
+                    new PathConstructor(),
+                    new CancellationManager(),
+                    jobStatus),
+                    jobStatus));
+
+            sourceTF.PropertyChanged += (s, e) => copyCancelCommand.TestCanExecute();
+            destTF.PropertyChanged += (s, e) => copyCancelCommand.TestCanExecute();
+
+            return copyCancelCommand;
+        }
+
+        private void initTextFields()
+        {
+            sourceTF = new TextField();
+            destTF = new TextField();
+        }
+
+        private void initValidators()
+        {
+            validators = new IValidator[2];
+            validators[0] = new PathValidator(sourceTF);
+            validators[1] = new PathValidator(destTF);
+        }
+
+        private void initOutput()
+        {
+            var outputList = new NotifyList<string>(new NotifyListEnumerator<string>());
+            output = new BasicOutput(outputList);
+        }
+
+        private void initJobStatus()
+        {
+            jobStatus = new JobStatus();
+        }
+
+        private void initSettingsManager()
+        {
+            settingsManager = new SettingsManager(overwriteVM);
         }
     }
 }

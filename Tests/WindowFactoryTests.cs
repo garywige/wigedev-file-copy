b4345bf6d9b4508672ada2569b51d08a82f365e7
garywige <91767400+garywige@users.noreply.github.com>
2022-06-04 19:20:22 -0700

    WindowFactory

---
+++ Tests/WindowFactoryTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WigeDev.View.Implementations;
+
+namespace Tests
+{
+    [TestClass]
+    public class WindowFactoryTests
+    {
+        private WindowFactory<FakeWindowAdapter> sut;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            sut = new();
+        }
+
+        [TestMethod]
+        public void CreateWindowIsNotNull()
+        {
+            var window = sut.CreateWindow();
+            Assert.IsNotNull(window);
+        }
+    }
+}
---
+++ View/Implementations/WindowFactory.cs
@@ -0,0 +1,12 @@
+﻿using WigeDev.View.Interfaces;
+
+namespace WigeDev.View.Implementations
+{
+    public class WindowFactory<T> : IWindowFactory<T> where T: IWindowAdapter, new()
+    {
+        public T CreateWindow()
+        {
+            return new T();
+        }
+    }
+}

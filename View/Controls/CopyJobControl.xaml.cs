41120e32872258b68728b850221359cafee4e784
garywige <91767400+garywige@users.noreply.github.com>
2022-06-06 07:02:15 -0700

    CopyJobControl

---
+++ View/Controls/CopyJobControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WigeDev.View.Controls
+{
+    /// <summary>
+    /// Interaction logic for CopyJobControl.xaml
+    /// </summary>
+    public partial class CopyJobControl : UserControl
+    {
+        public CopyJobControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

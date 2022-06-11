86cd2bfdf325ab77f2b3710ab689747f3a0d1c06
garywige <91767400+garywige@users.noreply.github.com>
2022-06-11 14:30:04 -0700

    WindowInitializer

---
+++ Init.Implementations/WindowInitializer.cs
@@ -0,0 +1,48 @@
+﻿using WigeDev.Init.Interfaces;
+using System.Windows;
+using WigeDev.View.Windows;
+using WigeDev.ViewModel.Interfaces;
+using WigeDev.Validation.Interfaces;
+
+namespace WigeDev.Init.Implementations
+{
+    public class WindowInitializer : IInitializer<Window>
+    {
+        protected IDictionary<string, ITextField> textFields;
+        protected IValidator validator;
+        protected IJobStatus jobStatus;
+        protected INotifyList<ICopyJobControlViewModel> jobList;
+
+        public Window Initialize()
+        {
+            textFields = new TextFieldInitializer().Initialize();
+            validator = new ValidatorInitializer(textFields["source"], textFields["destination"]).Initialize();
+            var output = new OutputInitializer().Initialize();
+            jobStatus = new JobStatusInitializer().Initialize();
+            var overwriteVM = new OverwriteVMInitializer(output).Initialize();
+            var settingsManager = new SettingsManagerInitializer(overwriteVM).Initialize();
+            jobList = new BatchJobListInitializer().Initialize();
+
+            // Main Window
+            return new MainWindow(
+                new FolderSelectionCVMInitializer("Source", textFields["source"], jobStatus).Initialize(),
+                new FolderSelectionCVMInitializer("Destination", textFields["destination"], jobStatus).Initialize(),
+                new CopyCancelCCVMInitializer(jobStatus, new CopyCancelCommandInitializer(validator, settingsManager, textFields["source"], textFields["destination"], output, jobStatus).Initialize()).Initialize(),
+                new OutputVMInitializer(output, jobStatus).Initialize(),
+                overwriteVM,
+                null, // TODO
+                new AddJobCCVMInitializer(addJobShowDialog, jobList).Initialize(),
+                new BatchListCVMInitializer(jobList).Initialize());
+        }
+
+        protected bool? addJobShowDialog(object? output)
+        {
+            var addCommand = new AddJobAddCommandInitializer(textFields["source"], textFields["destination"], validator).Initialize();
+            var cancelCommand = new AddJobCancelCommandInitializer().Initialize();
+            var window = new AddJobWindowInitializer(textFields, jobStatus, addCommand, cancelCommand).Initialize();
+            addCommand.SetExecute(new AddJobAddCommandExecuteInitializer(validator, window, textFields["source"], textFields["destination"], jobList).Initialize());
+            cancelCommand.SetExecute(() => window.Close());
+            return window.ShowDialog() == true;
+        }
+    }
+}
--- WigeDev File Copy/App.xaml.cs
+++ WigeDev File Copy/App.xaml.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Input;
-using WigeDev.Copier.Interfaces;
+﻿using System.Windows;
 using WigeDev.Init.Implementations;
-using WigeDev.Output.Interfaces;
-using WigeDev.Settings.Interfaces;
-using WigeDev.Validation.Interfaces;
-using WigeDev.View.Windows;
-using WigeDev.ViewModel.Implementations;
-using WigeDev.ViewModel.Interfaces;
 
 namespace WigeDev_File_Copy
 {
@@ -18,57 +8,14 @@ namespace WigeDev_File_Copy
     /// </summary>
     public sealed partial class App : Application
     {
-        private Window window;
-        private IJobStatus jobStatus;
-        private IDictionary<string, ITextField> textFields;
-        private IValidator validator;
-        private IOutput output;
-        private ISelectControlViewModel<ICopyStrategy> overwriteVM;
-        private ISettingsManager settingsManager;
-        private INotifyList<ICopyJobControlViewModel> jobList;
-
-        public App()
-        {
-            window = initializeWindow();
-        }
+        private Window? window;
 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-
+            window = new WindowInitializer().Initialize();
             window.Show();
         }
-
-        private Window initializeWindow()
-        {
-            textFields = new TextFieldInitializer().Initialize();
-            validator = new ValidatorInitializer(textFields["source"], textFields["destination"]).Initialize();
-            output = new OutputInitializer().Initialize();
-            jobStatus = new JobStatusInitializer().Initialize();
-            overwriteVM = new OverwriteVMInitializer(output).Initialize();
-            settingsManager = new SettingsManagerInitializer(overwriteVM).Initialize();
-            jobList = new BatchJobListInitializer().Initialize();
-
-            // Main Window
-            return new MainWindow(
-                new FolderSelectionCVMInitializer("Source", textFields["source"], jobStatus).Initialize(),
-                new FolderSelectionCVMInitializer("Destination", textFields["destination"], jobStatus).Initialize(),
-                new CopyCancelCCVMInitializer(jobStatus, new CopyCancelCommandInitializer(validator, settingsManager, textFields["source"], textFields["destination"], output, jobStatus).Initialize()).Initialize(),
-                new OutputVMInitializer(output, jobStatus).Initialize(),
-                overwriteVM,
-                null, // TODO
-                new AddJobCCVMInitializer(addJobShowDialog, jobList).Initialize(),
-                new BatchListCVMInitializer(jobList).Initialize());
-        }
         
-        private bool? addJobShowDialog(object? output)
-        {
-            var addCommand = new AddJobAddCommandInitializer(textFields["source"], textFields["destination"], validator).Initialize();
-            var cancelCommand = new AddJobCancelCommandInitializer().Initialize();
-            var window = new AddJobWindowInitializer(textFields, jobStatus, addCommand, cancelCommand).Initialize();
-            addCommand.SetExecute(new AddJobAddCommandExecuteInitializer(validator, window, textFields["source"], textFields["destination"], jobList).Initialize());
-            cancelCommand.SetExecute(() => window.Close());
-            return window.ShowDialog() == true;
-        }
     }
 }
